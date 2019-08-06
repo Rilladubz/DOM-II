a95f19031ee3e86199634837116188da64c46115
Samuel Torres <rilla@Samuels-iMac.local>
2019-08-06 18:59:38 -0400

    Add 7 evenlisteners

--- README.md
+++ README.md
@@ -17,15 +17,15 @@ Fun Bus wants you to make their site more interactive. They are relying on you t
 
 * [-] Submit a Pull-Request to merge <firstName-lastName> Branch into master (student's  Repo). **Please don't merge your own pull request**
 * [-] Add your project manager as a reviewer on the pull-request
-* [ ] Your project manager will count the project as complete by merging the branch back into master.
+* [-] Your project manager will count the project as complete by merging the branch back into master.
 
 ## Task 1: Set Up LESS Preprocessor
 
-* [ ] Verify that you have LESS installed correctly by running `lessc -v` in your terminal, if you don't get a version message back, reach out to your project manager for help.
+* [-] Verify that you have LESS installed correctly by running `lessc -v` in your terminal, if you don't get a version message back, reach out to your project manager for help.
 
 * [ ] Open your terminal and navigate to your preprocessing project by using the `cd` command
 
-* [ ] Once in your project's root folder, run the following command `less-watch-compiler less css index.less`
+* [-] Once in your project's root folder, run the following command `less-watch-compiler less css index.less`
 
 * [ ] Verify your compiler is working correctly by changing the `background-color` on the `html` selector to `red` in your `index.less` file.
 
--- js/index.js
+++ js/index.js
@@ -1,1 +1,55 @@
 // Your code goes here
+
+// 1 When bus/sand pic clicked size is halved
+const bus = document.querySelector('.bus');
+// console.log(bus);
+bus.addEventListener('click', (e) => {
+    bus.style.maxWidth='50%';
+});
+
+// 2 when mouse leaves bus image size returns to normal size
+bus.addEventListener('mouseover', (e) => {
+    bus.style.maxWidth='100%';
+});
+
+//3 when logo is double clicked nav color changes red
+const logo = document.querySelector('.logo-heading');
+// console.log(logo);
+const nav = document.querySelectorAll('.nav-link');
+// console.log(nav);
+logo.addEventListener('dblclick', (e) => { 
+    nav.forEach((navItem, i) => {
+        navItem.style.color='red';
+      });
+});
+
+//4 when mouseleaves first button paragraph displays none
+const firstButton = document.querySelector('.first');
+// console.log(firstButton);
+const firstPara = document.querySelector('.left p');
+// console.log(firstPara);
+firstButton.addEventListener('mouseleave', (e) => {
+    firstPara.style.display='none';
+});
+
+//5 when middle paragraph is clicked with any mouse btn, btn displays none.
+const secondButton = document.querySelector('.second');
+// console.log(secondButton);
+const secondPara = document.querySelector('.middle p');
+// console.log(secondPara);
+secondPara.addEventListener('mousedown', (e) => {
+    secondButton.style.display='none'; 
+});
+
+//6 when you scroll with the mouse wheel buttons displays none.
+const thirdButton = document.querySelector('.third');
+// console.log(thirdButton);
+const thirdPara = document.querySelector('.right p');
+// console.log(thirdPara);
+thirdPara.addEventListener('wheel', (e) => {
+    thirdButton.style.display='none'; 
+});
+
+7//
+
+
